b927ec3f7a4b20f437d1b69932f63e0dc1bfaf44
A-Bolf <hosc19@gmail.com>
2022-05-10 16:36:14 +0200

    Refactored Player and monster movement and combat, added damage modifier to weapon pickups and health modifiers to health pickups

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,26 +1,69 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.items.Consumable;
 import com.codecool.dungeoncrawl.logic.items.Item;
-import com.codecool.dungeoncrawl.logic.items.ItemType;
+import com.codecool.dungeoncrawl.logic.items.Weapon;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
-public class Player extends Actor {
+public class Player extends Actor implements CanPickupItems {
+    private Cell cell;
+    private ArrayList<Item> inventory = new ArrayList<>();
+    private ArrayList<Consumable> consumables = new ArrayList<>();
+    private ArrayList<Weapon> weapons = new ArrayList<>();
+
     public Player(Cell cell) {
-        super(cell,100,5);
+        super(cell, 100, 5);
+        this.cell = cell;
     }
-    private static LinkedList<Item> inventory= new LinkedList<>();
 
-    public static void pickUpItem(Item item){
-        inventory.add(item);
+    //    @Override
+    public void move(int dx, int dy) {
+        Cell nextCell = cell.getNeighbor(dx, dy);
+        if (nextCell.isWalkable()) {
+            if (nextCell.isAttackable()) {
+                combat(nextCell.getActor());
+                return;
+            }
+            if (nextCell.hasItem()) {
+                pickUpItem(nextCell);
+                nextCell.removeItem();
+            }
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
     }
-    public static LinkedList<Item> getInventory(){
-        return inventory;
+
+    public void combat(Actor enemy) {
+        enemy.damageHealth(super.getDamage());
+        damageHealth(enemy.getDamage());
     }
-    public void removeItem(Item item){
-        inventory.remove(item);
+
+    public void pickUpItem(Cell cell) {
+        Item item = cell.getItem();
+        if (item.getTileName().equals("food")) {
+            consumables.add((Consumable) item);
+        } else if (item.getTileName().equals("sword")) {
+            weapons.add((Weapon) item);
+        }
+        updateStats();
     }
+
+    private void updateStats() {
+        for (Weapon weapon : weapons) {
+            increaseDamage(weapon.getDamage());
+            inventory.add(weapon);
+        }
+        weapons.clear();
+        for (Consumable consumable : consumables) {
+            increaseHealth(consumable.getHealthValue());
+            inventory.add(consumable);
+        }
+        consumables.clear();
+    }
+
     public String getTileName() {
         return "player";
     }
