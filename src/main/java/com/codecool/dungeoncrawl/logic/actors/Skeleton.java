68ac29e9c7883427cd1a55ad932a17c6969d2cde
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-05-10 14:30:31 +0200

    Add random move

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -2,13 +2,29 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 
+import java.util.Random;
+
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
         super(cell);
     }
 
+    private Random random = new Random();
+
     @Override
     public String getTileName() {
         return "skeleton";
     }
+
+    public void moveSkeleton() {
+        int dx = random.nextInt(2) - 1;
+        int dy;
+        if (dx == 0) {
+            dy = random.nextInt(2) - 1;
+        } else {
+            dy = 0;
+        }
+        move(dx, dy);
+    }
 }
+
